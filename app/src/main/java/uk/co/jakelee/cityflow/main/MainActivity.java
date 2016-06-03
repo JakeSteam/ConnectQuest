d5297cdc99667a9472cc3a3d00e498007f53777a
Jake <jake_l@live.co.uk>
2016-06-03 19:55:09 +0100

    Initial commit.

---
+++ app/src/main/java/uk/co/jakelee/cityflow/main/MainActivity.java
@@ -0,0 +1,15 @@
+package uk.co.jakelee.cityflow.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uk.co.jakelee.cityflow.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
