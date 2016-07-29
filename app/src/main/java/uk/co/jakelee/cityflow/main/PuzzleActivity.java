d27ff6c97594f3f2793ab011adf9bc1a82e89e7e
Jake <bitbucket@jakelee.co.uk>
2016-07-29 22:10:17 +0100

    Added first draft of actually designed end level screen.

--- app/src/main/java/uk/co/jakelee/cityflow/main/PuzzleActivity.java
+++ app/src/main/java/uk/co/jakelee/cityflow/main/PuzzleActivity.java
@@ -304,12 +304,12 @@ public class PuzzleActivity extends Activity {
         ((ImageView) findViewById(R.id.starCompletion)).setImageResource(puzzle.hasCompletionStar() ? R.drawable.ui_star_achieved : R.drawable.ui_star_unachieved);
         ((ImageView) findViewById(R.id.starTime)).setImageResource(puzzle.hasTimeStar() ? R.drawable.ui_star_achieved : R.drawable.ui_star_unachieved);
         ((ImageView) findViewById(R.id.starMoves)).setImageResource(puzzle.hasMovesStar() ? R.drawable.ui_star_achieved : R.drawable.ui_star_unachieved);
-        ((TextView) findViewById(R.id.currentMoves)).setText(Integer.toString(movesMade > 0 ? movesMade : 0));
+        /*((TextView) findViewById(R.id.currentMoves)).setText(Integer.toString(movesMade > 0 ? movesMade : 0));
         ((TextView) findViewById(R.id.currentTime)).setText(timeInMilliseconds > 0 ? DateHelper.getPuzzleTimeString(timeInMilliseconds) : "0");
         ((TextView) findViewById(R.id.bestMoves)).setText(puzzle.getBestMoves() + (newBestMoves ? "*" : ""));
         ((TextView) findViewById(R.id.bestTime)).setText(DateHelper.getPuzzleTimeString(puzzle.getBestTime()) + (newBestTime ? "*" : ""));
         ((TextView) findViewById(R.id.parMoves)).setText(Integer.toString(puzzle.getParMoves()));
-        ((TextView) findViewById(R.id.parTime)).setText(DateHelper.getPuzzleTimeString(puzzle.getParTime()));
+        ((TextView) findViewById(R.id.parTime)).setText(DateHelper.getPuzzleTimeString(puzzle.getParTime()));*/
     }
 
     public void pausePuzzle(View v) {
