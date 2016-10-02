6f5dea4cd7fa158d062561d2efb40946a782e757
Jake <bitbucket@jakelee.co.uk>
2016-10-03 00:04:03 +0100

    Minor caching fix.

--- app/src/main/java/uk/co/jakelee/cityflow/Components/TextAwesome.java
+++ app/src/main/java/uk/co/jakelee/cityflow/Components/TextAwesome.java
@@ -3,35 +3,26 @@ package uk.co.jakelee.cityflow.components;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
-import android.util.LruCache;
 import android.widget.TextView;
 
-
 public class TextAwesome extends TextView {
+	private static Typeface mTypeface;
 
-	private final static String NAME = "FONTAWESOME";
-	private static LruCache<String, Typeface> sTypefaceCache = new LruCache<String, Typeface>(12);
-
-	public TextAwesome(Context context) {
-		super(context);
-		init();
+	public TextAwesome(final Context context) {
+		this(context, null);
 	}
 
-	public TextAwesome(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init();
+	public TextAwesome(final Context context, final AttributeSet attrs) {
+		this(context, attrs, 0);
 	}
 
-	public void init() {
-		Typeface typeface = sTypefaceCache.get(NAME);
+	public TextAwesome(final Context context, final AttributeSet attrs, final int defStyle) {
+		super(context, attrs, defStyle);
 
-		if (typeface == null) {
-			typeface = Typeface.createFromAsset(getContext().getAssets(), "fontawesome-webfont.ttf");
-			sTypefaceCache.put(NAME, typeface);
+		if (mTypeface == null) {
+			mTypeface = Typeface.createFromAsset(context.getAssets(), "fontawesome-webfont.ttf");
 		}
-
-		setTypeface(typeface);
-
+		setTypeface(mTypeface);
 	}
 
 }
